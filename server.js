4df37630b315c815bb3d9a4c06be00caac5165af
Jeffrey Shapiro <jeffreyhshapiro@gmail.com>
2017-02-20 00:26:47 -0500

    add production port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const randomWord = require('random-words');
 const defineWord = require('define-word');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('logic'));
 app.use(express.static('css'));
