b1fbc879d3165e872676a92e1886c712c7d9a435
Jeffrey Shapiro <jeffreyhshapiro@gmail.com>
2017-02-26 23:29:14 -0500

    db connection success

--- models/word_times.js
+++ models/word_times.js
@@ -1,9 +1,9 @@
 const sequelize = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  let WordTimes = sequelize.define('word_times', {
+  return WordTimes = sequelize.define('word_times', {
     word: {
-      type: sequelize.STRING
+      type: DataTypes.STRING
     }
   });
 }
--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const app = express();
 const randomWord = require('random-words');
 const defineWord = require('define-word');
 const Sequelize = require('sequelize');
-const wordTimes = require('./models/word_times.js');
+const models = require('./models');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('logic'));
 app.use(express.static('css'));
 
-let sequelize = new Sequelize('typefall', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres',
-}).authenticate().then(() => {
-  console.log('successful')
-}).catch((err) => {
-  console.log(`Unable to connect to database: ${err}`);
-});
+// let sequelize = new Sequelize('typefall', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// }).authenticate().then(() => {
+//   console.log('successful')
+// }).catch((err) => {
+//   console.log(`Unable to connect to database: ${err}`);
+// });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -36,10 +36,10 @@ app.get('/word', (req, res) => {
 
 app.post('/saveWordData', (req, res) => {
   console.log('right herrrrrr');
-})
+});
 
-wordTimes.sync().then(() => {
+models.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
-})
+});
