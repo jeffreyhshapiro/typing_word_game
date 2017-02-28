797c7459ba1c2d76cbc9253af4f3224333bdda6c
Jeffrey Shapiro <jeffreyhshapiro@gmail.com>
2017-02-27 22:18:08 -0500

    passing data to backend

--- logic/db.js
+++ logic/db.js
@@ -3,10 +3,11 @@ let db = (function($) {
   return {
     saveRound: (wordData) => {
       console.log('herrrr')
+      console.log(wordData)
       $.ajax({
         type: "POST",
         url: '/saveWordData',
-        data: wordData,
+        data: {wordData: wordData},
         success: (res) => {
           console.log('success');
         }
