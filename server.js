f50efbb176698da91d46bb32661d8ac0d46bac41
Jeffrey Shapiro <jeffreyhshapiro@gmail.com>
2017-05-17 11:34:58 -0400

    clean server file

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
-const session = require('express-session');
-const ip = require('request-ip');
-const os = require('os');
-const bp = require('body-parser');
-// const wd = require('word-definition');
 const Sequelize = require('sequelize');
 const models = require('./models');
 const routes = require('./routes/api.js')
-const dataHandler = require('./utility/data_handler.js');
-require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.use('/', routes);
